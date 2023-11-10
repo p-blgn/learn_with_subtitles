05389d290c228edcb3dd86e8e3575a7b9d48f7fb
Pierre <pierredu596002@yahoo.fr>
2023-11-10 17:46:04 +0100

    Changed deprecated functions of PyPDF2 in Dico.py

--- Dico.py
+++ Dico.py
@@ -6,11 +6,12 @@ Created on Sat Oct 29 14:11:57 2022
 """
 
 import PyPDF2
+
 temp = open('C:/Users/pierr/Documents/Python Scripts/Sous-titres/Dictionnaire Français-Allemand.pdf', 'rb')
-PDF_read = PyPDF2.PdfFileReader(temp)
-nombre_pages = PDF_read.getNumPages()
+PDF_read = PyPDF2.PdfReader(temp)
+nombre_pages = len(PDF_read.pages)
 page = ''
 for i in range(nombre_pages):    
-    page += PDF_read.getPage(i).extractText()
+    page += PDF_read.pages[i].extract_text()
 liste = page.split('\n')
 liste = liste[3:]
